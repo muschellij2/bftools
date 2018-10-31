2eb77b3884bdb4f10fcfbf0463f72c628dfda861
muschellij2 <muschellij2@gmail.com>
2018-10-31 16:36:07 -0400

    added the tests

--- tests/testthat/test-converter.R
+++ tests/testthat/test-converter.R
@@ -8,4 +8,6 @@ testthat::test_that("Just trying a simple convert", {
     res = bfconvert(file = file)
     testthat::expect_true(file.exists(res))
   }
+  testthat::expect_true(file.exists(bf_cmd("bfconvert")))
+
 })
